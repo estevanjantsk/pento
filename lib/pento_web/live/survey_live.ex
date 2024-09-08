436546743505ec087c105ed6c58aa229b52131e2
Estevan Jantsk <estevan.jantsk@scoro.com>
2024-09-08 14:24:26 +0300

    feat: Update survey live view and component
    The code changes in this commit update the survey live view and component. The survey live view now includes a hero component with the content "Survey" and a message asking users to fill out the survey. The survey live view also renders the demographic details if available, or displays a message indicating that the demographic form is coming soon.
    
    This commit improves the user experience of the survey feature and prepares for future enhancements related to demographics.

--- lib/pento_web/live/survey_live.ex
+++ lib/pento_web/live/survey_live.ex
@@ -1,7 +1,16 @@
 defmodule PentoWeb.SurveyLive do
   use PentoWeb, :live_view
 
+  alias PentoWeb.SurveyLive.Component
+  alias PentoWeb.DemographicLive
+  alias Pento.Survey
+
+  @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, socket |> IO.inspect() |> assign_demographic()}
+  end
+
+  defp assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
+    assign(socket, demographic: Survey.get_demographic_by_user(current_user))
   end
 end
