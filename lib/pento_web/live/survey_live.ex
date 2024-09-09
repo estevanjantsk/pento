b35fc99406e30e6c3d55a3c8358d23c13ca055fb
Estevan Jantsk <estevan.jantsk@scoro.com>
2024-09-09 22:26:13 +0300

    refactor: Improve handling of demographic form in live view

--- lib/pento_web/live/survey_live.ex
+++ lib/pento_web/live/survey_live.ex
@@ -3,11 +3,15 @@ defmodule PentoWeb.SurveyLive do
 
   alias PentoWeb.SurveyLive.Component
   alias PentoWeb.DemographicLive
-  alias Pento.Survey
+  alias Pento.{Survey, Catalog}
+  alias PentoWeb.RatingLive
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket |> IO.inspect() |> assign_demographic()}
+    {:ok,
+     socket
+     |> assign_demographic()
+     |> assign_products()}
   end
 
   @impl true
@@ -17,6 +21,14 @@ defmodule PentoWeb.SurveyLive do
 
   defp assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
     assign(socket, demographic: Survey.get_demographic_by_user(current_user))
+  end
+
+  defp assign_products(%{assigns: %{current_user: current_user}} = socket) do
+    assign(socket, :products, list_products(current_user))
+  end
+
+  defp list_products(user) do
+    Catalog.list_products_with_user_ratings(user)
   end
 
   defp handle_demographic_created(socket, demographic) do
