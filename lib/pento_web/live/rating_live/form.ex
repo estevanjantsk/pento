68ef5e7699f3a327cf2804792b3abc22a0920400
Estevan Jantsk <estevan.jantsk@scoro.com>
2024-09-10 23:18:30 +0300

    feat: Save rating in rating form live view
    This commit adds the `save_rating` function to the `PentoWeb.RatingLive.Form` module. The function is called when the "save" event is triggered and it saves the rating using the `Survey.create_rating` function. If the rating is successfully created, it updates the product's ratings and sends a `:created_rating` message. If there is an error with the changeset, it assigns the changeset to the socket.
    
    This code change enhances the functionality of the rating form live view and improves the user experience by allowing users to save their ratings.

--- lib/pento_web/live/rating_live/form.ex
+++ lib/pento_web/live/rating_live/form.ex
@@ -15,7 +15,7 @@ defmodule PentoWeb.RatingLive.Form do
   end
 
   def handle_event("save", %{"rating" => rating_params}, socket) do
-    {:noreply, socket}
+    {:noreply, save_rating(socket, rating_params)}
   end
 
   def assign_rating(%{assigns: %{current_user: user, product: product}} = socket) do
@@ -28,5 +28,20 @@ defmodule PentoWeb.RatingLive.Form do
 
   def clear_form(%{assigns: %{rating: rating}} = socket) do
     assign_form(socket, Survey.change_rating(rating))
+  end
+
+  def save_rating(
+        %{assigns: %{product_index: product_index, product: product}} = socket,
+        rating_params
+      ) do
+    case Survey.create_rating(rating_params) do
+      {:ok, rating} ->
+        product = %{product | ratings: [rating]}
+        send(self(), {:created_rating, product, product_index})
+        socket
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        assign_form(socket, changeset)
+    end
   end
 end
